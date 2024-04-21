9f36977832edc655b45e611ba2a523faccb13176
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2024-04-21 11:56:32 +0800

    Update mulai-install.sh

--- mulai-install.sh
+++ mulai-install.sh
@@ -2,24 +2,24 @@
 
 clear
 echo
-echo -e "\e[93m memindai pembaruan paket."
+echo -e "\e[93mMmemindai pembaruan paket."
 apt update
 apt upgrade -y
 
 echo
-echo -e "\e[93m menginstall paket yang di butuhkan."
+echo -e "\e[93mNenginstall paket yang di butuhkan."
 pkg install wget proot -y
 
 echo
-echo -e "\e[93m mendapatkan data"
+echo -e "\e[93mMendapatkan data"
 
 git clone https://github.com/MiftahulKhoiri/Os-linux-scrip.sh/blob/main/Kali-Linux.sh
 
 echo
-echo -e "\e[93m memberikan izin pada data."
+echo -e "\e[93mMemberikan izin pada data."
 chmod +x Kali-Linux.sh
 
 echo
-echo -e "\e[93m script ini akan menginstall kalilinux di termux secara otomatis."
+echo -e "\e[93mScript ini akan menginstall kalilinux di termux secara otomatis."
 Kali-Linux.sh
 
