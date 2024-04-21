c09b280ae3cff4fba747bcfd72db0437682f40b3
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2024-04-21 19:16:08 +0800

    Update script-pembaruan.sh

--- script-pembaruan.sh
+++ script-pembaruan.sh
@@ -1,6 +1,9 @@
 #! bin/bash
 
 echo "memeriksakan pembaruan "
-rm -rf 
+rm -rf Os-linux-di-termux-/
+git clone https://github.com/MiftahulKhoiri/Os-linux-scrip.sh.git
+
+echo "memperoses file "
 
 cd /data/data/com.termux/files/home
