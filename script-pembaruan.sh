b98b1bd540ce164997e895cb2e200cb281f20811
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2024-04-29 02:09:28 +0800

    Update script-pembaruan.sh

--- script-pembaruan.sh
+++ script-pembaruan.sh
@@ -17,6 +17,8 @@ echo "memulai download file"
 git clone https://github.com/MiftahulKhoiri/Os-linux-scrip.sh.git
 sleep 3
 echo "memperoses file "
+cd /data/data/com.termux/files/home/
+rm dir -rf linux/
 cd /data/data/com.termux/files/home/Os-linux-di-termux-/
 chmod +x mulai-install.sh
 chmod +x script-pembaruan.sh
