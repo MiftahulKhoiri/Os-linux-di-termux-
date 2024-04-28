1126d3381d810ad2f3164e74c463f9328bf97379
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2024-04-29 02:19:06 +0800

    Update script-pembaruan.sh

--- script-pembaruan.sh
+++ script-pembaruan.sh
@@ -13,20 +13,25 @@ icon="="
 sleep 2
 echo "terdapat pembaruan file "
 sleep 3
+clear 
+
 echo "memulai download file"
 git clone https://github.com/MiftahulKhoiri/Os-linux-scrip.sh.git
 sleep 3
+clear
+
 echo "memperoses file "
 cd /data/data/com.termux/files/home/
 rm dir -rf linux/
+mkdir linux
 cd /data/data/com.termux/files/home/Os-linux-di-termux-/
 chmod +x mulai-install.sh
 chmod +x script-pembaruan.sh
-cd /data/data/com.termux/files/home
-mkdir linux
 cp script-pembaruan.sh /data/data/com.termux/files/home/linux/
 cp mulai-install.sh /data/data/com.termux/files/home/linux/
 sleep 3
+clear
+cd /data/data/com.termux/files/home
 echo "membersihkan file sampah "
 rm dir -rf Os-linux-di-termux-/
 
